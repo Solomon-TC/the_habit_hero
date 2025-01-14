92a17415af4da54382fc45a600c5ab01a4f0ef18
capell.solomon <capell.solomon@gmail.com>
2025-01-14 19:43:22 +0000

    fix: enable svg support in next/image

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,8 @@ const nextConfig = {
         hostname: 'uhmqsszvollkbxcspyqe.supabase.co',
       },
     ],
+    dangerouslyAllowSVG: true,
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
 }
 
